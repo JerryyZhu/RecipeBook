71e3e98c7a763b5a9b841cf72a30a95db8429b66
Jerry Zhu <jerryzhu96@hotmail.com>
2021-10-05 22:47:02 +1100

    initial scaffold

---
+++ Controllers/RecipesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using recipes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeBook.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        
+        public RecipesController()
+        {
+            
+        }
+
+        // GET: api/<RecipesController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        // POST api/<RecipesController>
+        [HttpPost]
+        public IActionResult Post([FromForm] Recipe recipeObj)
+        {
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        // DELETE api/<RecipesController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok("Record deleted");
+        }
+    }
+}
---
+++ Models/Recipe.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace recipes.Models
+{
+    using Newtonsoft.Json;
+    public class Recipe
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "image")] 
+        public string image { get; set; }
+        [JsonProperty(PropertyName = "source")]
+        public string source { get; set; }
+
+        [JsonProperty(PropertyName = "sourceUrl")]
+        public string sourceUrl { get; set; }
+
+        [JsonProperty(PropertyName = "calories")]
+        public string calories { get; set; }
+
+        [JsonProperty(PropertyName = "healthLabels")]
+        public string healthLabels { get; set; }
+
+        [JsonProperty(PropertyName = "cautions")]
+        public string cautions { get; set; }
+
+        [JsonProperty(PropertyName = "ingredientLines")]
+        public string ingredientLines { get; set; }
+
+        [JsonProperty(PropertyName = "ingredients")]
+        public string ingredients { get; set; }
+
+        [JsonProperty(PropertyName = "totalWeight")]
+        public string totalWeight { get; set; }
+
+        [JsonProperty(PropertyName = "totalTime")]
+        public string totalTime { get; set; }
+
+        [JsonProperty(PropertyName = "dishType")]
+        public string dishType { get; set; }
+
+        [JsonProperty(PropertyName = "cuisineType")]
+        public string cuisineType { get; set; }
+
+        [JsonProperty(PropertyName = "totalNutrients")]
+        public string totalNutrients { get; set; }
+
+        [JsonProperty(PropertyName = "totalDaily")]
+        public string totalDaily { get; set; }
+
+        [JsonProperty(PropertyName = "digest")]
+        public string digest { get; set; }
+
+
+        [JsonProperty(PropertyName = "IsComplete")]
+        public string IsComplete { get; set; }
+
+
+    }
+}
